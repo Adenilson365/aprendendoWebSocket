cfe8ac808f7e6a804d9d7654d0a6605e52f8b714
Adenilson(De) <adekonzelmann@gmail.com>
2023-09-12 21:10:29 -0300

    Criando usuários e estilizando conforme contexto

--- app.js
+++ app.js
@@ -17,9 +17,13 @@ const io = socketIo(server);
 
 io.on('connection', (socket)=>{
     console.log("New Connection");
+    
     socket.on('new_message', (data)=>{
-        messages.push(data.message);
+        console.log(data);
+        console.log(data.msg, data.user);
+        messages.push(data);
         io.emit('update_messages', messages)
+        console.log(messages);
     })
 
 })
--- public/chat.js
+++ public/chat.js
@@ -1,31 +1,52 @@
 const socket = io('http://localhost:3000')
 
 socket.on('update_messages', (messages)=>{
-    console.log('recebido')
     updateMessagesOnScreen(messages);
 })
 
+let user = null;
 
 function updateMessagesOnScreen(messages){
     const div_messages = document.querySelector('#messages');
-
-    let list_messages = '<ul>';
+    let list_messages = '<ul class="list-class">'
     messages.forEach(message => {
-        list_messages += `<li>${message}</li>`
+        if(user == message.user){
+        list_messages += `<li class="user-principal"><div>${message.user}:${message.msg}</div></li>`
+        }else{
+            list_messages += `<li class ="user-secondary"><div >${message.user}:${message.msg}</div></li>`
+        }
     });
-
     list_messages += '</ul>';
     div_messages.innerHTML = list_messages;
-}
+
+    
+
+ }
 
 document.addEventListener('DOMContentLoaded', ()=>{
     const form = document.querySelector('#form-msg');
     form.addEventListener('submit', (e)=>{
         e.preventDefault();
+        if(!user){
+            alert("Defina Usuário");
+            return;
+        }
         const message = document.forms['msg-form']['msg'].value;
-        document.forms['msg-form']['msg'].value ='';
-        socket.emit('new_message',{msg:message});
-       
+        if(!message ==''){
+            document.forms['msg-form']['msg'].value ='';
+            socket.emit('new_message',{user:user,msg:message});  
+        }
+    })
+
+    const userForm = document.querySelector('#user-form');
+    userForm.addEventListener('submit', (e)=>{
+        e.preventDefault();
+         user = document.forms['user-form']['user'].value;
+        if(!user){
+            alert("Defina Usuario");
+            return;
+        }
+        userForm.parentNode.removeChild(userForm);
     })
 })
 
